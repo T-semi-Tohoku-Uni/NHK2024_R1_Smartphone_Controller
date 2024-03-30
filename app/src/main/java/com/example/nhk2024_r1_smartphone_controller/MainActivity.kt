4931603b6839a8ee087ce447bcf359c2557ad453
Some One <sagankeigo17@gmail.com>
2024-03-31 01:49:51 +0900

    Add UDP

--- app/src/main/java/com/example/nhk2024_r1_smartphone_controller/MainActivity.kt
+++ app/src/main/java/com/example/nhk2024_r1_smartphone_controller/MainActivity.kt
@@ -2,9 +2,18 @@ package com.example.nhk2024_r1_smartphone_controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.KeyEvent
+import android.widget.Button
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
 import kotlin.concurrent.thread
 
 
@@ -12,11 +21,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var controllerObject: ControllerObject
     private lateinit var hostName: String
     private val port = 12345
+    private val socket = DatagramSocket()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize controllerObject
         this.controllerObject = ControllerObject(
             vx = 127,
             vy = 127,
@@ -29,11 +40,13 @@ class MainActivity : AppCompatActivity() {
             btnR1 = false
         )
         this.hostName = "192.168.10.106"
-        thread {
-            for (i in 0..10) {
-                RaspiRepository().sendControllerData(hostName, port, this.controllerObject)
-            }
-        }
+
+//        // For debug
+//        val button = findViewById<Button>(R.id.button)
+//        button.setOnClickListener {
+//            Log.d("UDP_SEND", "ボタンがクリックされました。UDPパケットを送信します。")
+//            RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+//        }
     }
 
     // For analog input
@@ -43,14 +56,12 @@ class MainActivity : AppCompatActivity() {
         val axisX = event.getAxisValue(MotionEvent.AXIS_X)   // left stick horizontal
         val axisY = event.getAxisValue(MotionEvent.AXIS_Y)   // left stick vertical
         val axisZ = event.getAxisValue(MotionEvent.AXIS_Z)   // right stick horizontal
-
         // val axisRZ = event.getAxisValue(MotionEvent.AXIS_RZ) // right stick vertical
 
         this.controllerObject.setRobotXYVelocity(axisX, axisY)
         this.controllerObject.setAngularVelocity(axisZ)
 
-        // TODO: Send data to Raspberrypi
-        RaspiRepository().sendControllerData(hostName, port, this.controllerObject)
+        RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
 
         return super.onGenericMotionEvent(event)
     }
@@ -62,26 +73,35 @@ class MainActivity : AppCompatActivity() {
         when (keyCode) {
             KeyEvent.KEYCODE_BUTTON_A -> {
                 this.controllerObject.setButtonA(true)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
             KeyEvent.KEYCODE_BUTTON_B -> {
                 this.controllerObject.setButtonB(true)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
             KeyEvent.KEYCODE_BUTTON_X -> {
                 this.controllerObject.setButtonX(true)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
             KeyEvent.KEYCODE_BUTTON_Y -> {
                 this.controllerObject.setButtonY(true)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
             KeyEvent.KEYCODE_BUTTON_L1 -> {
                 this.controllerObject.setButtonL1(true)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
-            KeyEvent.KEYCODE_BUTTON_L2 -> {
+            KeyEvent.KEYCODE_BUTTON_R1 -> {
                 this.controllerObject.setButtonR1(true)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
         }
-
-        // TODO: Send data to Raspberrypi
-        RaspiRepository().sendControllerData(hostName, port, this.controllerObject)
 
         return super.onKeyDown(keyCode, event)
     }
@@ -93,26 +113,35 @@ class MainActivity : AppCompatActivity() {
         when (keyCode) {
             KeyEvent.KEYCODE_BUTTON_A -> {
                 this.controllerObject.setButtonA(false)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
             KeyEvent.KEYCODE_BUTTON_B -> {
                 this.controllerObject.setButtonB(false)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
             KeyEvent.KEYCODE_BUTTON_X -> {
                 this.controllerObject.setButtonX(false)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
             KeyEvent.KEYCODE_BUTTON_Y -> {
                 this.controllerObject.setButtonY(false)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
             KeyEvent.KEYCODE_BUTTON_L1 -> {
                 this.controllerObject.setButtonL1(false)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
             KeyEvent.KEYCODE_BUTTON_R1 -> {
                 this.controllerObject.setButtonR1(false)
+                RaspiRepository().sendControllerData(this.hostName, this.port, this.socket, this.controllerObject)
+                return true
             }
         }
-
-        // TODO: Send data to Raspberrypi
-        RaspiRepository().sendControllerData(hostName, port, this.controllerObject)
 
         return super.onKeyUp(keyCode, event)
     }
--- app/src/main/java/com/example/nhk2024_r1_smartphone_controller/RaspiRepository.kt
+++ app/src/main/java/com/example/nhk2024_r1_smartphone_controller/RaspiRepository.kt
@@ -1,5 +1,9 @@
 package com.example.nhk2024_r1_smartphone_controller
 
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import java.io.IOException
 import java.net.DatagramPacket
@@ -12,14 +16,14 @@ class RaspiRepository(){
     // TODO: implement Check Connection and send PING for 1 sec until press disconnect button
 
     // TODO: implement sendControllerData
-    fun sendControllerData(hostName: String, port: Int, ctrData: ControllerObject): Boolean {
+    fun sendControllerData(hostName: String, port: Int, socket: DatagramSocket, ctrData: ControllerObject): Boolean {
         try {
-            val socket = DatagramSocket()
-            socket.use {
+            Log.d("RaspiRepository", "sendControllerData")
+            CoroutineScope(Dispatchers.IO).launch {
                 val buffer: ByteArray = Json.encodeToString(ControllerObject.serializer(), ctrData).toByteArray()
                 val packet = DatagramPacket(buffer, buffer.size, InetAddress.getByName(hostName), port)
                 socket.send(packet)
-            }
+            }.start()
         } catch (e: UnknownHostException) {
             print("UnknownHostException")
             return false
